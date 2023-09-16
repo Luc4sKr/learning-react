67415967d762c29cec922922ab06416d8ce66d6e
Luc4sKr <lucaskreuch77@gmail.com>
2023-09-16 19:19:43 -0300

    state

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+import { Button } from "@mui/material";
 import { HelloWorld } from "../../shared/components/helloWorld/HelloWorld";
 
+import "./style.css";
+
 export const Home = () => {
+	const [clicks, setClicks] = useState(0);
+
+	const handleClick = () => {
+		setClicks(clicks + 1);
+	}
+
 	return (
-		<div>
-			<HelloWorld />
-		</div>
+		<>
+			<div className="top-container">
+				<HelloWorld />
+				<Button variant="contained" onClick={handleClick}>
+					Clicks: {clicks}
+				</Button>
+			</div>
+		</>
 	);
 }
